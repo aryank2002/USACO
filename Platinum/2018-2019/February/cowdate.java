c7a952a5ea35883fb5e57f108cb97fc370b64ee8
aryank2002 <43621811+aryank2002@users.noreply.github.com>
2020-03-30 12:00:06 -0700

    Update cowdate.java

--- Platinum/2018-2019/February/cowdate.java
+++ Platinum/2018-2019/February/cowdate.java
@@ -1,1 +1,37 @@
+import java.io.*;
 
+public class cowdate {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("cowdate.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cowdate.out")));
+		
+		int N = Integer.parseInt(br.readLine());
+		double[] prob = new double[N];
+		for (int i = 0; i < N; i++){
+			prob[i] = (double) Integer.parseInt(br.readLine()) / 1000000;
+		}
+		
+		double max = 0;
+		double sum = 0;
+		double prod = 1;
+		int l = 0;
+		int r = 0;
+		
+		while (l < N){
+			while (r < N && sum < 1){
+				sum += prob[r] / (1 - prob[r]);
+				prod *= (1 - prob[r]);
+				r++;
+			}
+			max = Math.max(max, sum * prod);
+			sum -= prob[l] / (1 - prob[l]);
+			prod /= (1 - prob[l]);
+			l++;
+		}
+		
+		pw.println((int) (1000000 * max));
+		pw.close();
+		br.close();
+	}
+}
